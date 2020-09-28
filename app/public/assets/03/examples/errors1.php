b902d387526a960914e7c0d0730da987471b22cc
Joris Maervoet <joris.maervoet@odisee.be>
2020-09-28 10:41:26 +0200

    fixing some type declaration

--- app/public/assets/03/examples/errors1.php
+++ app/public/assets/03/examples/errors1.php
@@ -3,7 +3,7 @@
 	error_reporting(E_ALL); // set error reporting on
     ini_set('display_errors', 'on'); // show the errors on screen
 
-	function divide($numerator, $denominator) {
+	function divide(float $numerator, float $denominator) {
 		return $numerator / $denominator;
 	}
 
