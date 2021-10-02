58c6b6cb1981780b502916f0f5bbd5ae383a18a8
Joris Maervoet <joris.maervoet@odisee.be>
2021-10-02 12:48:06 +0200

    slightly updating PHP advanced slides

---
+++ app/public/assets/04/examples/oo09.php
@@ -0,0 +1,27 @@
+<?php
+
+class Weight {
+
+	private float $weight;
+	private ?DateTime $registrationDate;
+
+	public function __construct(float $weight, ?DateTime $registrationDate = null) {
+		$this->weight = $weight;
+        $this->registrationDate = $registrationDate;
+	}
+
+	public function getRegistrationDate() : ?DateTime {
+	    return $this->registrationDate;
+    }
+}
+
+
+?>
+<pre><?php
+
+$myWeight1 = new Weight(82.5);
+var_dump($myWeight1?->getRegistrationDate()?->format('Y-m-d'));
+$myWeight2 = new Weight(78.5, new DateTime('now'));
+var_dump($myWeight2?->getRegistrationDate()?->format('Y-m-d'));
+
+?></pre>
--- app/public/assets/04/examples/spl_exception_02.php
+++ app/public/assets/04/examples/spl_exception_02.php
@@ -2,7 +2,7 @@
 
 	try {
 		$di = new DirectoryIterator('/this/path/does/not/exist');
-	} catch (RuntimeException $e) {
+	} catch (UnexpectedValueException $e) {
 		echo 'There was an error: <br />' . $e->getMessage();
 	}
 
