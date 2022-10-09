1bb1c8d96de0ec1723233c71521465e15d310062
Joris Maervoet <joris.maervoet@odisee.be>
2022-10-09 23:59:13 +0200

    adding slides/examples on present-day forms with javascript

---
+++ app/public/assets/06/examples/form_webservice.php
@@ -0,0 +1,28 @@
+<?php
+
+$name = isset($_POST['name']) ? (string) $_POST['name'] : false;
+
+if ($name !== false) {
+    // validation
+
+    $errorList = [];
+
+    if (trim($name) == '') {
+        $errorList[] = 'Please enter your name';
+    }
+
+    if (!$errorList) {
+        // do (database) action
+        // ...
+        echo 'Name has been registered';
+        exit; // HTTP response is sent with status code 200
+    } else {
+        http_response_code(422); // 422 Unprocessable Entity
+        echo implode(';', $errorList);
+        exit; // HTTP response is sent with status code 422
+    }
+} else {
+    http_response_code(400); // 400 Bad Request
+    echo 'Malformed request';
+    exit; // HTTP response is sent with status code 400
+}
