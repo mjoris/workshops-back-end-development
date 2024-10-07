11368820725831d0f564d95fdd7efa7f6c310e6a
Joris Maervoet <joris.maervoet@odisee.be>
2024-10-07 14:36:41 +0200

    add 8.3's new randomizer functions

--- app/public/assets/04/examples/randomizer.php
+++ app/public/assets/04/examples/randomizer.php
@@ -2,11 +2,15 @@
 
 $randomizer = new Random\Randomizer();
 
-echo $randomizer->getInt(1, 100); // 42
+echo $randomizer->getInt(1, 100);
 echo '<br>';
-echo $randomizer->shuffleBytes('lorem ipsum'); // "ols mpeurim"
+echo $randomizer->getFloat(0.1, 0.3); // default interval boundary: ClosedOpen
 echo '<br>';
-print_r($randomizer->shuffleArray(['apple', 'banana', 'orange'])); // ['orange', 'apple', 'banana']
+echo $randomizer->getBytesFromString('abcdefghij', 16);
+echo '<br>';
+echo $randomizer->shuffleBytes('lorem ipsum');
+echo '<br>';
+print_r($randomizer->shuffleArray(['apple', 'banana', 'orange']));
 echo '<br>';
 $randomizer = new Random\Randomizer(new Random\Engine\Mt19937());
-echo $randomizer->getInt(1, 100); // 68
+echo $randomizer->getInt(1, 100);
