be7e67538566b857bb2d16f41265c524f1dbae64
Joris Maervoet <joris.maervoet@odisee.be>
2024-10-07 15:49:15 +0200

    apply PHP-at-the-top HTML-at-the-bottom structure to demo upload script

--- app/public/assets/05/examples/upload.php
+++ app/public/assets/05/examples/upload.php
@@ -5,33 +5,42 @@
  * @author Bramus Van Damme <bramus.vandamme@odisee.be> / Joris Maervoet <joris.maervoet@odisee.be>
  */
 
+$uploaded = false;
+$moved = false;
+$errorMessage = '';
+
 // a file has been POSTed (could be empty) and it has been uploaded correctly
 if (isset($_FILES['avatar']) && ($_FILES['avatar']['error'] === UPLOAD_ERR_OK)) {
 
-    // get file info
-    echo '<p>Uploaded file: ' . $_FILES['avatar']['name'] . '</p>' . PHP_EOL;
-    echo '<p>Temp location: ' . $_FILES['avatar']['tmp_name'] . '</p>' . PHP_EOL;
-    echo '<p>Size: ' . $_FILES['avatar']['size'] . '</p>' . PHP_EOL;
+    $uploaded = true;
 
     // check file extension
-    if (in_array((new SplFileInfo($_FILES['avatar']['name']))->getExtension(), array('jpeg', 'jpg', 'png', 'gif'))) {
+    if (in_array((new SplFileInfo($_FILES['avatar']['name']))->getExtension(), ['jpeg', 'jpg', 'png', 'gif'])) {
 
         // store file in this folder
         $moved = @move_uploaded_file($_FILES['avatar']['tmp_name'], __DIR__ . DIRECTORY_SEPARATOR . $_FILES['avatar']['name']);
 
         if ($moved) {
-            // show image
-            echo '<p><img src="' . $_FILES['avatar']['name'] . '" alt="" /><p>';
+            // do more things ...
         } else {
-            echo('<p>Error while saving file in the uploads folder</p>');
+            $errorMessage = 'Error while saving file in the uploads folder';
         }
     } else {
-        echo('<p>Invalid file extension. Only .jpeg, .jpg, .png or .gif allowed</p>');
+        $errorMessage = 'Invalid file extension. Only .jpeg, .jpg, .png or .gif allowed';
     }
 }
 
 ?>
-
+<?php if ($uploaded) { ?>
+    <p>Uploaded file: <?php echo $_FILES['avatar']['name']; ?></p>
+    <p>Temp location: <?php echo $_FILES['avatar']['tmp_name']; ?></p>
+    <p>Size: <?php echo $_FILES['avatar']['size']; ?></p>
+    <?php if ($moved) { ?>
+        <p><img src="<?php echo $_FILES['avatar']['name']; ?>" alt=""/><p>
+    <?php } else { ?>
+        <p><?php echo $errorMessage ?></p>
+    <?php } ?>
+<?php } ?>
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
     <label for="avatar">Avatar: </label>
     <input type="file" name="avatar" id="avatar"/><br/>
