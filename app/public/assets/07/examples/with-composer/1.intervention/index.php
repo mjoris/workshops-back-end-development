33e1e5d54518ae75fdfc98e165bd0fec83fea1f6
Joris Maervoet <joris.maervoet@odisee.be>
2024-10-14 16:27:31 +0200

    update to Intervention Image v3

--- app/public/assets/07/examples/with-composer/1.intervention/index.php
+++ app/public/assets/07/examples/with-composer/1.intervention/index.php
@@ -3,20 +3,21 @@
 // require composer autoloader
 require_once __DIR__ . '/vendor/autoload.php';
 
-// import the Intervention Image Manager Class
-use Intervention\Image\ImageManagerStatic as Image;
+// import Intervention Image Manager Class and Gd Driver
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
 
-// open an image file
-$image = Image::make('assets/procla_ict_oct_2022.jpg');
+// create image manager with desired driver
+$manager = new ImageManager(new Driver());
+
+// read image from file system
+$image = $manager->read('assets/procla_ict_oct_2022.jpg');
 
 // resize the image to a width of 1024 pixels and maintain aspect ratio
-$image->resize(1024, null, function ($constraint) {
-    $constraint->aspectRatio();
-});
+$image->scale(1024, null);
 
 // insert a watermark
-// As of 15 Oct. 2022, this method generates 2 deprecated errors, as Intervention 2.* is not optimized for PHP 8.1
-@ $image->insert('assets/odisee.png', 'bottom-right');
+$image->place('assets/odisee.png', 'bottom-right');
 
 // finally we save the image as a new file
 $image->save('output/procla_ict_oct_2022_watermark.jpg');
