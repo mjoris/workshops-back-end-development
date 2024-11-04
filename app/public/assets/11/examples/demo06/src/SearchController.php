2e0b130a9af3e7048709e192adae04263cf78b5d
Joris Maervoet <joris.maervoet@odisee.be>
2024-11-04 20:18:45 +0100

    replace config.php by dotenv system and add type declarations in demo06

--- app/public/assets/11/examples/demo06/src/SearchController.php
+++ app/public/assets/11/examples/demo06/src/SearchController.php
@@ -7,24 +7,29 @@ class SearchController
 
     public function __construct()
     {
+        // load .env
+        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+        $dotenv->load();
+        $dotenv->required('BASE_PATH');
+
         // initiate DB connection
 
         // bootstrap Twig
         $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../templates');
         $this->twig = new \Twig\Environment($loader);
         $function = new \Twig\TwigFunction('url', function ($path) {
-            return BASE_PATH . $path;
+            return $_ENV['BASE_PATH'] . $path;
         });
         $this->twig->addFunction($function);
     }
 
-    public function show()
+    public function show(): void
     {
-        echo $this->twig->render('search.twig' , ['term' => '', 'results' => []]);
+        echo $this->twig->render('search.twig', ['term' => '', 'results' => []]);
     }
 
-    public function showResults($term)
+    public function showResults(string $term): void
     {
-        echo $this->twig->render('search.twig' , ['term' => $term, 'results' => ['dummy1', 'dummy2']]);
+        echo $this->twig->render('search.twig', ['term' => $term, 'results' => ['dummy1', 'dummy2']]);
     }
 }
