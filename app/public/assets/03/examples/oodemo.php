d7699b38e1370395bee61355a9643ef461508b54
Joris Maervoet <joris.maervoet@odisee.be>
2025-09-26 15:46:43 +0200

    update php basics slides to PHP 8.4

--- app/public/assets/03/examples/oodemo.php
+++ app/public/assets/03/examples/oodemo.php
@@ -6,7 +6,7 @@ echo($myDateOfBirth->getTimestamp() . '<br>' . PHP_EOL); // method call
 $w3cFormat = DateTime::W3C; // class constant
 echo($myDateOfBirth->format($w3cFormat) . '<br>' . PHP_EOL);
 
-$date = DateTime::createFromFormat('j-M-Y', '01-Oct-2024'); // static method call
+$date = DateTime::createFromFormat('j-M-Y', '01-Oct-2025'); // static method call
 $duration = $date->diff($myDateOfBirth);
 echo($duration->days); // accessing a property
 
